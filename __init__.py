89518132a323479833023999981a261dc17a78bb
filfo131 <83786249+filfo131@users.noreply.github.com>
2022-10-03 15:21:56 +0200

    build

--- __init__.py
+++ __init__.py
@@ -4,5 +4,11 @@ from flask import Flask
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'RANDOMWORD'
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(view, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/auth/')
+
     return app
 
